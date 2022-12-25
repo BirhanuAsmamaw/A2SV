7f38da3abbfde4dc1195bf9cf09d433611ec2658
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2022-12-25 17:01:26 +0530

    Time: 1003 ms (29.93%), Space: 14.9 MB (95.53%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            another = target - nums[i]
+            if another in nums:
+                try:
+                    j = nums.index(another)
+                    if i != j:
+                        return [i, j]
+                except ValueError as e:
+                    # There has no item in list
+                    continue
+        return []
