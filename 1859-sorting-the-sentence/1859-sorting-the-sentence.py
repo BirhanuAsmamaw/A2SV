4b63c4ce04c1da3d508262f9395256c78e3dd66e
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2022-12-26 16:59:26 +0530

    Time: 24 ms (98.16%), Space: 13.9 MB (57.66%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        arr = s.split()
+        num=[None]* len(arr)
+        for i in range(len(arr)):
+            last= int(arr[i][-1])
+            num[last-1] = arr[i][:-1]
+            final= ' '
+        return final.join(num)
+        
+            
+            
+            
