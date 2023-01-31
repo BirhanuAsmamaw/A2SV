0e7bdb32aebcdfb5b1f14f036ca52a112f3c0051
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-01-31 23:06:25 +0530

    Time: 41 ms (36.86%), Space: 13.9 MB (14.09%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def recurse(x, n):
+            if x== 0:
+                return 0
+            if n== 0:
+                return 1
+            
+            result = recurse(x, n//2)
+            result = result * result
+            return x * result if n % 2 else result
+        
+        result = recurse(x, abs(n))
+        return result if n >= 0 else 1 / result
+        
