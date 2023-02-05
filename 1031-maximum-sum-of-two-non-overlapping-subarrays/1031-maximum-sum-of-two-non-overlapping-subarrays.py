cb5ee8ed7711c3dfb7a136d391f29e377df5c58c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-02-05 22:03:27 +0530

    Time: 579 ms (19.80%), Space: 13.9 MB (99.24%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int) -> int:
+        n = len(nums)
+        first_sum = [-math.inf]*n
+        second_sum = [-math.inf]*n
+        
+        for i in range(n):
+            if i>=firstLen-1:
+                first_sum[i] = sum(nums[i-firstLen+1:i+1])
+            if i >= secondLen -1:
+                second_sum[i] = sum(nums[i-secondLen+1: i+1])
+        max_sum = 0
+
+        for i in range(firstLen-1, n):
+            for j in range(secondLen-1,n):
+                if j < i - firstLen+1 or j - secondLen+1 > i:
+                    max_sum = max(first_sum[i]+second_sum[j], max_sum)
+
+        return max_sum
