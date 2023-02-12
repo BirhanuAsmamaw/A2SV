67bfaa7ba8950e23300ae1321769ccd24dbd2c67
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-02-12 18:56:50 +0530

    Time: 42 ms (66.43%), Space: 13.9 MB (13.06%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        result=[]
+        counter=collections.Counter(s)
+
+        change = set()
+        length = 0
+        for i,current in enumerate(s):
+            length += 1
+            change.add(current)
+
+            
+            counter[current] -= 1 
+            if counter[current]==0:
+                change.remove(current)
+                if len(change)==0:
+                    result.append(length)
+                    length=0
+        return result
+        
