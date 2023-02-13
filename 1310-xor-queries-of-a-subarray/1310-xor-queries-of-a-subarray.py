7ba3447aca5e446f1e8befc33d2f4ba3ab0eaf2c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-02-13 18:30:46 +0530

    Time: 389 ms (83.51%), Space: 28.8 MB (86.02%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        
+        final=[]
+        
+        for i in range(1,len(arr)):
+            arr[i] ^= arr[i-1]
+        
+        for j in queries:
+            if j[0] == 0:
+                final.append(arr[j[1]])
+            else:
+                temp=arr[j[0]-1] ^ arr[j[1]]
+                final.append(temp)
+        return final
+        
