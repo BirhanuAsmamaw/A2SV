0fe92f23a05706237b2a10a91b46b20174082ed5
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-02-19 19:40:22 +0530

    Time: 69 ms (68.63%), Space: 25.9 MB (87.13%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen_hash = set()
+        result = set()
+        
+        for ed in range(len(s) - 9):
+            current = s[ed : ed + 10]
+            if current in seen_hash:
+                result.add(current)
+            seen_hash.add(current)
+        
+        return list(result)
+        
