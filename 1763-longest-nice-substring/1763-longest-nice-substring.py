ce337e7875fd7a8f6a0da1ba8d3dcf44a6b9f87b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-02-25 00:14:10 +0530

    Time: 153 ms (36.22%), Space: 13.8 MB (68.43%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        Sub_String = ''
+        Nice_SubString = ''
+       
+        for en in range(len(s)):
+            for start in range(en + 1):
+                Sub_String = s[start: en + 1]
+                
+                if (len(set(Sub_String.lower()))) == (len(set(Sub_String))) // 2:
+                    Nice_SubString = max(Nice_SubString,Sub_String,key = len)
+                    
+        return Nice_SubString
+            
+           
+        
