f2ee292fdd5295dbcc6a2df9304bcff31386d090
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-03-23 23:30:14 +0530

    Time: 1317 ms (83.37%), Space: 39.7 MB (8.43%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        array = [0] * (len(s) + 1)
+        
+        for start,end,direction in shifts:
+            if direction == 1:
+                array[start] += 1
+                array[end + 1] -= 1
+            else:
+                array[start] -= 1
+                array[end + 1] += 1
+        array.pop()
+        for i in range(1,len(s)):
+            array[i] += array[i-1]
+        
+        ord_s = [ord(i) for i in s] 
+        for j in range (len(s)):
+            ord_s[j] += array[j]
+            
+        return ''.join([chr((i - 97)  % 26 + 97) for i in ord_s])
+            
+                
+            
+        
+        
+        
+        
+        
+        
+        
