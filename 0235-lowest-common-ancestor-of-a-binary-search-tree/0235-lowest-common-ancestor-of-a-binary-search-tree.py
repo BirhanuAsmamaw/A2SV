28c718081986f595162250426c058c687e328caf
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-03-30 00:49:21 +0530

    Time: 88 ms (28.22%), Space: 18.8 MB (57.88%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if (p.val <= root.val and q.val >= root.val ) or (p.val >= root.val and q.val <= root.val):
+            return root
+        
+        if p.val < root.val:
+            return self.lowestCommonAncestor(root.left , p ,q)
+        if p.val > root.val:
+            return self.lowestCommonAncestor(root.right , p ,q)
+          
+            
