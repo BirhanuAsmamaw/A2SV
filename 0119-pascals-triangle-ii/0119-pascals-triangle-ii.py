bf7b61e6d6ce9e3eba4b496b014501a026f98b16
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-04-02 23:04:00 +0530

    Time: 24 ms (96.20%), Space: 13.9 MB (48.34%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = []
+        for i in range(rowIndex+1):
+            ladder = []
+            for j in range(i+1):
+                if j==0 or j==i:
+                    ladder.append(1)
+                else:
+                    ladder.append(result[i-1][j-1] + result[i-1][j]) 
+            result.append(ladder)
+        return result[-1]
