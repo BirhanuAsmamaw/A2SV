0abdacf25d198bf2e736bdee964ac115d1230ecd
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-04-13 00:51:51 +0530

    Time: 171 ms (45.73%), Space: 15.6 MB (23.35%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dicti = defaultdict(int)
+        n = len(nums)
+        for j in nums:
+            dicti[j] += 1
+            if dicti[j] > (n /2):
+                return j
+            
+                      
