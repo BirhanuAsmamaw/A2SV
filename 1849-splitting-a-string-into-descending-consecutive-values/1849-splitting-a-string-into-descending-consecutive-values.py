08a16ab039c6481bbf5ab143860e9d7d51265967
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-04-23 19:31:52 +0530

    Time: 40 ms (39.29%), Space: 13.9 MB (57.47%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -0,0 +1,17 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+        def dfs(idx,left):
+            if idx == len(s):
+                return True
+            for j in range(idx,len(s)):
+                rt_val = int(s[idx:j+1])
+                if left == rt_val + 1 and dfs(j+1, rt_val):
+                    return True
+            return False
+        
+        for i in range(len(s)-1):
+            lt_val = int(s[:i+1])
+            
+            if dfs(i+1,lt_val):
+                return True
+        return False
