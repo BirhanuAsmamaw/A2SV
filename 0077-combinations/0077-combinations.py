dc2d50c981f1ba92c5767043c0726eddcb1cadc9
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-04-23 09:00:26 +0530

    Time: 406 ms (54.90%), Space: 16 MB (41.24%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        
+        def backTracking(start , combination):
+            if len(combination) == k:
+                result.append(combination.copy())
+                return
+            for i in range(start,n + 1):
+                combination.append(i)
+                backTracking(i + 1, combination)
+                combination.pop()
+       
+        backTracking(1, [])
+        return result
+            
+        
