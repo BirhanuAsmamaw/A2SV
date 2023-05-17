8bf01c506a7baa6f8599104337ab5f5d0b37b20d
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2023-05-17 09:15:24 -0900

    Time: 185 ms (9.87%), Space: 17.6 MB (20.77%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,25 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        importance = 0
+        def dfs(id):
+            nonlocal importance
+            for i in employees:
+                if i.id != id:
+                    continue
+                importance += i.importance
+
+                subordinates = len(i.subordinates)
+                for j in range(subordinates):
+                    dfs(i.subordinates[j])
+        dfs(id)
+        return importance   
+    
