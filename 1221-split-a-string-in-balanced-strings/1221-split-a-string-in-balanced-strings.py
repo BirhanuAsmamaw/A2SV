85eb09a26ecdee7ad4a9359a87ba73ef4297ec8e
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-02 11:02:25 -0800

    Time: 55 ms (7.40%), Space: 17.3 MB (12.87%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        d = {'R':0, 'L':0}
+        result = 0
+        for i in s:
+            if i == 'R':
+                d['R'] += 1
+            else:
+                d['L'] += 1
+            if d['R'] == d['L'] and d['R'] != 0:
+                result += 1
+        return result
