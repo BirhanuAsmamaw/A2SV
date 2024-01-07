6d9c62a101c902130e31cf31ce079db6b8f2c777
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-07 10:38:25 -0800

    Time: 39 ms (94.98%), Space: 17.2 MB (17.76%) - LeetHub

---
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        ranges.sort()
+
+        for start, end in ranges:
+            if start <= left and left <= end:
+                left = end + 1
+
+        return left > right
