f3b539dd520f4d66b10c2a975fab0238420b1d2a
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-15 09:23:49 -1000

    Update 0100-same-tree.py

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -13,7 +13,6 @@ class Solution:
         
         if p.val != q.val:
             return False
-        
         return self.isSameTree(p.left , q.left) and self.isSameTree(p.right , q.right)
         
-        
+         
