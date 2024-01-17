369dfe48979350ea81df249dcf0ee401112cb0fa
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-17 09:52:15 -1000

    Update 0021-merge-two-sorted-lists.py

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -15,9 +15,8 @@ class Solution:
             else:
                 tail.next = list2
                 list2 = list2.next
-            
             tail = tail.next
-        
+            
         if list1:
             tail.next = list1
         elif list2:
