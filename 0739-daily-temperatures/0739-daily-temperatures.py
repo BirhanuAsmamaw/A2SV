4cb8db52005540a25cad397649e9e260ea583503
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-30 09:56:47 -1000

    Update 0739-daily-temperatures.py

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,7 +2,6 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result = [0] * len(temperatures) 
         stack = []
-        
         for i,val in enumerate(temperatures):
             while stack and val > stack[-1][0]:
                 stack_Val, stack_Index = stack.pop()
