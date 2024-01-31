f38c54bd016cce07064b4f633580557b520a410b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-01-31 08:59:29 -1000

    Update 0235-lowest-common-ancestor-of-a-binary-search-tree.py

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,5 +14,6 @@ class Solution:
             return self.lowestCommonAncestor(root.left , p ,q)
         if p.val > root.val:
             return self.lowestCommonAncestor(root.right , p ,q)
+            
           
             
