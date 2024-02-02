74c77a83fd9df6d7af2c2e5767fd8f2ae901186b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-02-02 01:32:23 -1000

    Update 0136-single-number.py

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,6 +2,5 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
         for i in nums:
-            result = result ^ i
-            
+            result = result ^ i 
         return result
