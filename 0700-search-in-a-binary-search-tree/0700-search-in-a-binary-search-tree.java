3cb351601a44b5c3288f02945766f29168009278
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-02-20 20:55:13 -0800

    Time: 0 ms (100.00%), Space: 45 MB (71.77%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+if (root == null) {
+            return null;
+        }
+
+        // If current node's value matches, return it (the whole subtree rooted here)
+        if (root.val == val) {
+            return root;
+        }
+
+        // Search in appropriate subtree based on comparison
+        if (val < root.val) {
+            return searchBST(root.left, val); // Search in left subtree
+        } else {
+            return searchBST(root.right, val); // Search in right subtree
+        }
+        
+    }
+}
