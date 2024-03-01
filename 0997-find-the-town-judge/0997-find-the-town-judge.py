3715dd684db0e773a7a3f639078973e8189da340
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-01 10:20:14 -0800

    Time: 608 ms (46.05%), Space: 21.6 MB (45.80%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -2,10 +2,9 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trusted_by = [0] * (n + 1)
         for a, b in trust:
-            trusted_by[a] -= 1  # a trusts someone, so they can't be the elder
-            trusted_by[b] += 1  # b is trusted by someone
+            trusted_by[a] -= 1  
+            trusted_by[b] += 1  
 
-        # Find the person who is trusted by everyone else and trusts nobody
         for i in range(1, n + 1):
             if trusted_by[i] == n - 1:
                 return i
