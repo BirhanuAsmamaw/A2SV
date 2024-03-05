758366f17835faa89c4e57b10e7886b4643da06e
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-05 09:26:58 -1000

    Update 0724-find-pivot-index.py

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,7 +4,6 @@ class Solution:
         left_sum = 0
         for i in range(len(nums)):
             right_sum = total_sum - nums[i] - left_sum
-            
             if left_sum == right_sum:
                 return i
             left_sum += nums[i]
