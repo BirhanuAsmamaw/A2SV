9fa6c4687e2ce9b4f3fac44421b75af78bce5f49
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-15 09:09:46 -0900

    Update 0304-range-sum-query-2d-immutable.py

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -12,6 +12,7 @@ class NumMatrix:
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         result = self.dp[row2 + 1][col2 + 1] - self.dp[row2 + 1][col1] - \
                  self.dp[row1][col2 + 1] + self.dp[row1][col1]
+
         return result
 
 # Your NumMatrix object will be instantiated and called as such:
