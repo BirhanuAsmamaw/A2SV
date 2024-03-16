609a9fffca889f9f666e0c39d37a95ae3534c493
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-16 09:50:10 -0900

    Update 0234-palindrome-linked-list.py

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -7,7 +7,6 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         result = []
         n = head
-        
         while(n):
             result.append(n.val)
             n = n.next
