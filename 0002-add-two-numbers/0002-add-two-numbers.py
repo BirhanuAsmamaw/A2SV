d77284becbd93ee491151f33637995807183ef9c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-17 11:18:32 -0900

    Update 0002-add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,8 +7,8 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         linked = ListNode()
         current = linked
-        
         pointer = 0
+
         while l1 or l2 or pointer:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
