c5e86c372a9ff403be829cda333b179657eca374
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-17 11:22:03 -0900

    Update 0002-add-two-numbers.py

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -21,7 +21,6 @@ class Solution:
             current = current.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            
         return linked.next
         
         
