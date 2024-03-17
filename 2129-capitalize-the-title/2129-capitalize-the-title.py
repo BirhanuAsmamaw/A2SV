9e70ca7f2a85b780dd997d22fa4f1c2d2675f9ca
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-17 11:23:09 -0900

    Update 2129-capitalize-the-title.py

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -2,11 +2,9 @@ class Solution:
     def capitalizeTitle(self, title: str) -> str:
         title = title.split()
         result = ""
-        
         for i in range(len(title)):
             if len(title[i]) < 3:
                 result = result + title[i].lower() + " "
             else:
-                result = result + title[i].capitalize() + " "
-                
+                result = result + title[i].capitalize() + " "  
         return result[:-1]
