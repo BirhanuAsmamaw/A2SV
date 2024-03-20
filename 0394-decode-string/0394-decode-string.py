ba151a4d0673676935ecead94e05ad898a266538
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-20 13:15:16 -0900

    Update 0394-decode-string.py

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -2,18 +2,14 @@ class Solution:
     def decodeString(self, s: str) -> str:
         def helper(i,strng,l):
             if i >= len(s):
-                return ""
-				
+                return ""		
             if s[i]=="]":
                 l[0] = i 
-                return ""
-				
+                return ""		
             if s[i]=="[":
-                return int(strng) * helper(i+1,"",l) + helper(l[0]+1, "" ,l)
-				
+                return int(strng) * helper(i+1,"",l) + helper(l[0]+1, "" ,l)		
             if s[i].isnumeric():
-                return helper(i + 1,strng + s[i],l)
-				
+                return helper(i + 1,strng + s[i],l)		
             else:
                 return s[i] + helper(i+1,strng,l)
 				
