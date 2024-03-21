38c7c2204cfe888eddfcef7934e49f21411eedbf
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-21 10:25:21 -0900

    Update 0238-product-of-array-except-self.py

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,10 +3,12 @@ class Solution:
         n = len(nums)
         result = [1] * n
         preFix = 1
+
         for i in range(n):
             result[i] = preFix
             preFix *= nums[i]
         postFix = 1
+
         for i in range(n - 1, -1, -1):
             result[i] *= postFix
             postFix *= nums[i]
