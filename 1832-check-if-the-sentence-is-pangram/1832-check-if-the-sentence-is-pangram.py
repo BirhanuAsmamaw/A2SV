b4fbca02dc28142a371df36471317913ba49273d
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-24 08:59:13 -0900

    Update 1832-check-if-the-sentence-is-pangram.py

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,8 +1,8 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        
         unique_letters = set()
         for char in sentence:
             if char.islower():
                 unique_letters.add(char)
+
         return len(unique_letters) == 26
