e4e63d80365fa66335c3f5fc8391dfe28d779065
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-24 08:35:11 -0900

    Update 0342-power-of-four.py

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -3,6 +3,5 @@ class Solution:
         if n == 1 or n == 4:
             return True
         elif n > 4 and n % 4 == 0:
-            return self.isPowerOfFour(n/4)
-            
+            return self.isPowerOfFour(n/4)   
         return False
