a6bf2ff089fc985119526ef563fd969dd6eb9da6
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-03-24 08:58:32 -0900

    Update 0746-min-cost-climbing-stairs.py

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -2,12 +2,9 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         if len(cost) <= 1:
             return 0
-        
         dp = [0] * len(cost)
         dp[0] = cost[0]
         dp[1] = cost[1]
-        
         for i in range(2, len(cost)):
             dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        
         return min(dp[-1], dp[-2])
