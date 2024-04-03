ea1a8a60ec8ea9130acf98b232c0dd1c607e9e28
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-03 09:18:34 -0900

    Update 0703-kth-largest-element-in-a-stream.py

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -10,6 +10,7 @@ class KthLargest:
         heapq.heappush(self.minHeap, val)
         if len(self.minHeap) > self.k:
             heapq.heappop(self.minHeap)
+            
         return self.minHeap[0]
         
 # Your KthLargest object will be instantiated and called as such:
