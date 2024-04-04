41d1d1ff036ff21df4a537a0fa8ed6936bfafbfa
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-04 10:13:29 -0900

    Update 0771-jewels-and-stones.py

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -3,6 +3,5 @@ class Solution:
         Tot_jewel = 0
         for i in stones:
             if i in jewels:
-                Tot_jewel += 1
-                
+                Tot_jewel += 1  
         return Tot_jewel
