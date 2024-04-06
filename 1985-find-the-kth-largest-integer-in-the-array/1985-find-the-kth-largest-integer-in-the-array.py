a49bedd92cf360896b662ee82d7e0fed81a637b4
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-06 09:32:34 -0900

    Update 1985-find-the-kth-largest-integer-in-the-array.py

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -2,7 +2,6 @@ class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
         maxHeap = [-int(i) for i in nums]  # the negative is because of python has not maxheap.so, we change the max to min by -ve sign
         heapq.heapify(maxHeap)
-        
         while k > 1:
             heapq.heappop(maxHeap)
             k -= 1 
