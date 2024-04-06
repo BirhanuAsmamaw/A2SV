2248098180d2da5e0c16b5ed29cf59bed0b0670f
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-06 09:32:15 -0900

    Update 1763-longest-nice-substring.py

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -6,7 +6,6 @@ class Solution:
         for en in range(len(s)):
             for start in range(en + 1):
                 Sub_String = s[start: en + 1]
-                
                 if (len(set(Sub_String.lower()))) == (len(set(Sub_String))) // 2:
                     Nice_SubString = max(Nice_SubString,Sub_String,key = len)
         return Nice_SubString
