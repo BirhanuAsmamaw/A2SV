faabd409a0556eaac4b3fa625bdcf0990bf4c37a
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-08 01:08:15 -0700

    Time: 308 ms (51.99%), Space: 24.6 MB (22.43%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        queue = deque()
+        queue.append((original,cloned))
+
+        while queue:
+            origion, clone = queue.popleft()
+            if origion == target:
+                return clone
+
+            if origion.left:
+                queue.append((origion.left,clone.left))
+
+            if origion.right:
+                queue.append((origion.right,clone.right))
+        return None
