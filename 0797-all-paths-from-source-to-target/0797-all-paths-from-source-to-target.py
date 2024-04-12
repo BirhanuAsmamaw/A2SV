98ba40e705875d07984ca116ffa4ac3264cbd8ca
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-12 13:01:53 -0700

    Time: 82 ms (14.65%), Space: 18.4 MB (16.62%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,12 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        queue =[[0]]
+        target= len(graph) -1 
+        result = []        
+        while queue:
+            temp = queue.pop(0)
+            if temp[-1] == target:
+                result.append(temp)            
+            for neighbor in graph[temp[-1]]:
+                queue.append( temp +[neighbor])       
+        return result
