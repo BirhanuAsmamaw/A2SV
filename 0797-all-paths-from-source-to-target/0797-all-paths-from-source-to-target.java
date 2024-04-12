fd2ca032b79cadac28dd84a4a9fa5623dccce75d
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-12 13:20:09 -0700

    Time: 12 ms (6.12%), Space: 46.3 MB (48.25%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        int target = graph.length - 1;
+        Queue<List<Integer>> queue = new LinkedList<>();
+        List<Integer> initialPath = new ArrayList<>();
+        initialPath.add(0);
+        queue.add(initialPath);
+        
+        while (!queue.isEmpty()) {
+            List<Integer> temp = queue.poll();
+            if (temp.get(temp.size() - 1) == target) {
+                result.add(new ArrayList<>(temp));
+            }
+            for (int neighbor : graph[temp.get(temp.size() - 1)]) {
+                List<Integer> newPath = new ArrayList<>(temp);
+                newPath.add(neighbor);
+                queue.add(newPath);
+            }
+        }
+        
+        return result;
+    }
+}
