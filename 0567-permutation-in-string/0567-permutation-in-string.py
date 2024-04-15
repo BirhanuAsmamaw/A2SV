138c9c90373fa0a0b0e1548b81648b3557ef59a8
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-15 09:04:35 -0900

    Update 0567-permutation-in-string.py

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,11 +3,11 @@ class Solution:
         sn = len(s1)
         pn = len(s2)
         dict_p = Counter(s1)
+        
         for i in range(pn-sn+1):
             dict_s = Counter(s2[i : sn + i])
             if dict_s == dict_p:
-                return True
-                
+                return True   
         return False
         
         
