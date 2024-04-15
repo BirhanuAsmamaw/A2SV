01a7a9dd9e6c3c155779bfa054dee42b9c8e1c67
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-15 09:04:54 -0900

    Update 1431-kids-with-the-greatest-number-of-candies.py

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -3,9 +3,11 @@ class Solution:
         result = []
         n = len(candies)
         maxi = max(candies)
+        
         for i in range(n):
             if candies[i] + extraCandies >= maxi:
                 result.append(True)
             else:
                 result.append(False)
+                
         return result            
