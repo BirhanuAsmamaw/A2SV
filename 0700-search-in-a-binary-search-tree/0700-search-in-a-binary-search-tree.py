02d83cdd8f79afa5996814f2327b8efa07593be8
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-16 10:12:46 -0900

    Update 0700-search-in-a-binary-search-tree.py

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -11,6 +11,7 @@ class Solution:
             return None
         if root.val == val:
             return root
+            
         if val < root.val:
             return self.searchBST(root.left, val)
         else:
