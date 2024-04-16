dc4a1736c40ec7cf6469d682da0b3ef9dc74dab6
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-16 10:20:28 -0900

    Update 1984-minimum-difference-between-highest-and-lowest-of-k-scores.py

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -4,7 +4,6 @@ class Solution:
         start = 0
         n = len(nums)
         answer = inf
-        
         for end in range(k-1,n ):
             answer = min(answer, nums[end]- nums[start])
             start += 1
