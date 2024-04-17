b1f0d80ca754d560d8427147f4abe72b8155c594
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-17 09:33:04 -0900

    Update 0238-product-of-array-except-self.py

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,7 +7,7 @@ class Solution:
             result[i] = preFix
             preFix *= nums[i]
         postFix = 1
-
+        
         for i in range(n - 1, -1, -1):
             result[i] *= postFix
             postFix *= nums[i]
