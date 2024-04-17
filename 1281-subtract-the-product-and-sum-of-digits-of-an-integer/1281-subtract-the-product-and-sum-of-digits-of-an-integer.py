2959346a342961a9b39664e06604f0c5903e0d83
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-17 09:33:43 -0900

    Update 1281-subtract-the-product-and-sum-of-digits-of-an-integer.py

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -2,7 +2,6 @@ class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         Sum = 0
         multiple = 1
-
         while n > 0:
             digit = n % 10 
             Sum += digit
