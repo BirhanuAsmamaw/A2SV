db8b4fa8cb6cd384a05c31c3dc085917298a6ab0
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-17 09:33:21 -0900

    Update 0304-range-sum-query-2d-immutable.py

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -8,7 +8,6 @@ class NumMatrix:
                                 self.dp[row - 1][col] + \
                                 self.dp[row][col - 1] - \
                                 self.dp[row - 1][col - 1]
-        
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         result = self.dp[row2 + 1][col2 + 1] - self.dp[row2 + 1][col1] - \
                  self.dp[row1][col2 + 1] + self.dp[row1][col1]
