46bce6e518713b822ea2db6a2f5378c948169b9c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-19 04:41:50 -0900

    Update 0387-first-unique-character-in-a-string.py

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -6,6 +6,7 @@ class Solution:
                 dictionary[i]+=1
             else:
                 dictionary[i]=1
+
         for i in dictionary:
             if dictionary[i]==1:
                 return s.index(i)
