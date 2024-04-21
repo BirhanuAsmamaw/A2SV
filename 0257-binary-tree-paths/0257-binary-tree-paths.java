2c7205c78e69cb56bf213db4c5ee023e1b74ae7d
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-21 05:42:29 -0900

    Time: 7 ms (60.69%), Space: 42.3 MB (79.19%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        traverse(root, "", paths);
+        return paths;
+    }
+
+    private void traverse(TreeNode node, String path, List<String> paths) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) { // leaf node
+            paths.add(path + node.val);
+            return;
+        }
+        traverse(node.left, path + node.val + "->", paths);
+        traverse(node.right, path + node.val + "->", paths);
+    }
+}
