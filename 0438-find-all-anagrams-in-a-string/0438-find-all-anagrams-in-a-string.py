30e4c0989dbdee4e7e8c5a3302ee9fa973c2d6af
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-21 10:20:11 -0900

    Update 0438-find-all-anagrams-in-a-string.py

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,11 +4,11 @@ class Solution:
         sn = len(s)
         pn= len(p)
         dict_p =Counter(p)
-        
         for i in range(sn - pn + 1):
             dict_s =Counter(s[i : pn + i])
             if dict_s == dict_p:
-                result.append(i)   
+                result.append(i)
+                
         return result
         
         
