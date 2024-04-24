8213f9b19065fb0ea2250a83dd4934e92e166979
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-24 09:28:22 -0900

    Time: 9 ms (5.87%), Space: 44.8 MB (9.64%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        while (set.size() != factorial(nums.length)) {
+            shuffleArray(nums);
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            set.add(permutation);
+        }
+        return new ArrayList<>(set);
+    }
+    
+    private int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+    
+    private void shuffleArray(int[] array) {
+        Random rnd = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+}
