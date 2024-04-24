0e0d4fc95afcb84c58fad349db3112e226910a49
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-24 09:30:42 -0900

    Update 0997-find-the-town-judge.py

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -4,9 +4,7 @@ class Solution:
         for a, b in trust:
             trusted_by[a] -= 1  
             trusted_by[b] += 1  
-
         for i in range(1, n + 1):
             if trusted_by[i] == n - 1:
-                return i
-                
+                return i        
         return -1
