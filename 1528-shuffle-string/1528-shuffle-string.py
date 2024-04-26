8c84ce0044fddc44b14a9e64b7183a143c9accde
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-26 10:35:54 -0900

    Update 1528-shuffle-string.py

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         shuffled = [''] * len(s)
+        
         for i, char in enumerate(s):
             shuffled[indices[i]] = char
         return ''.join(shuffled)
