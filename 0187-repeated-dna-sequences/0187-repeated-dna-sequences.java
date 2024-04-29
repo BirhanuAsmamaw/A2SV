df21b8619bc959b5bed38f7c319e7f89d228b40d
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-29 10:36:21 -0900

    Time: 17 ms (76.07%), Space: 53.2 MB (68.41%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> L1 = new HashSet<>();
+        Set<String> L2 = new HashSet<>();
+        int l = s.length();
+        if (l <= 10)
+            return new ArrayList<>(L1);
+        for (int i = 0; i <= l - 10; i++) {
+            String stri = s.substring(i, i + 10);
+            if (L1.contains(stri) && !L2.contains(stri)) {
+                L2.add(stri);
+            } else if (!L1.contains(stri)) {
+                L1.add(stri);
+            }
+        }
+        return new ArrayList<>(L2);
+    }
+}
