384ab1a96669e28f17d7faaa94fb7f119ba96c4b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-04-30 11:24:53 -0900

    Update 1876-substrings-of-size-three-with-distinct-characters.py

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -4,5 +4,6 @@ class Solution:
         for i in range(len(s) - 2):
             if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
                 count += 1
+                
         return count
         
