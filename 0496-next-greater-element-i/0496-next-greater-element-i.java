21ef288503f36218f102d6b8bcc385f483a2bbe3
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-02 10:59:22 -0900

    Time: 9 ms (5.34%), Space: 43.3 MB (75.87%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        List<Integer> resultList = new ArrayList<>();
+        for (int i : nums1) {
+            int count = 0;
+            for (int j : nums2) {
+                if (i == j) {
+                    for (int k = indexOf(nums2, j) + 1; k < nums2.length; k++) {
+                        if (nums2[k] > j) {
+                            count++;
+                            resultList.add(nums2[k]);
+                            break;
+                        }
+                    }
+                    if (count == 0) {
+                        resultList.add(-1);
+                    }
+                }
+            }
+        }
+
+        // Convert List<Integer> to int[]
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+
+    // Custom implementation of indexOf for arrays
+    public static int indexOf(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
