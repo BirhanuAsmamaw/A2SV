6bfee3b86cb9938ef3e88c77ee684c9cf5c98f4a
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-07 09:38:35 -0900

    Update 2108-find-first-palindromic-string-in-the-array.py

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
+        
         for i in words:
             palindromic = ''
             for j in range(len(i)):
                 palindromic = i[j] + palindromic 
                 if palindromic == i:
-                    return i
-                    
+                    return i        
         return ''
