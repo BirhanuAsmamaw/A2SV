c615da3d5eb8d25ae9314568419da91564a5ff70
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-10 09:10:18 -0900

    Time: 1 ms (75.59%), Space: 41.1 MB (86.88%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int count = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                count = count * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(count);
+                stringStack.push(currentString);
+                currentString = new StringBuilder();
+                count = 0;
+            } else if (ch == ']') {
+                int repeatCount = countStack.pop();
+                StringBuilder decodedString = stringStack.pop();
+                for (int i = 0; i < repeatCount; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(ch);
+            }
+        }
+        return currentString.toString();
+    }
+}
