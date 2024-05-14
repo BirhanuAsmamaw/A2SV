57b21085047cd7d90fd3ddeb65964b7708f787d9
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-14 00:42:27 -0900

    Update 2942-find-words-containing-character.py

--- 2942-find-words-containing-character/2942-find-words-containing-character.py
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -1,6 +1,5 @@
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-        
         result = []
         for i,j in enumerate(words):
             if x in j:
