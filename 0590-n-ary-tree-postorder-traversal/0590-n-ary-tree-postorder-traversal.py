512a1ef8ef859c7a7668d4dfadb45c71a263c77b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-24 03:00:58 -0900

    Update 0590-n-ary-tree-postorder-traversal.py

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -9,7 +9,6 @@ class Node:
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         result = []
-
         def traverse(node):
             if node:
                 for child in node.children:
