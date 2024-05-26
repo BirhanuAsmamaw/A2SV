a7a0fb864e70df586ffdcfde5a34bae8602151e8
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-26 10:38:59 -0900

    Update 0204-count-primes.py

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,10 +1,11 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
+        
         if n==0 or n==1:
             return 0
         prime=[True]*n
         prime[0]=prime[1]=False
-        
+
         count =0
         for i in range(2, n):
             if prime[i]:
