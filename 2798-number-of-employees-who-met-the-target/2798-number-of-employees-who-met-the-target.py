8b0152caa21459ae1946c2883c0b4668d1082735
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-05-30 10:23:56 -0900

    Update 2798-number-of-employees-who-met-the-target.py

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -1,5 +1,6 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        
         result=0
         for i in range(len(hours)):
             if hours[i]>=target:
