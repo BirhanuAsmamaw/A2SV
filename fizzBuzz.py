8f1d6c7fe494085790c3067d99e20d8ee5b015fa
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-04 02:40:32 -0900

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,5 +1,6 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+        
         output=[]
         for i in range(1,n+1):
             if i %3 == 0 and i%5 == 0:
@@ -11,7 +12,6 @@ class Solution:
             else:
                 output.append(str(i))        
         return output
-
 fb=Solution()
 fb.fizzBuzz(5)
                 
