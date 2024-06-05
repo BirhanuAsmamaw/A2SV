88ca7038b53e5031aa2eaf36659d2c9f4ae719c8
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-05 02:47:18 -0900

    Update 1423-maximum-points-you-can-obtain-from-cards.py

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -3,7 +3,6 @@ class Solution:
         l, r = 0, len(cardPoints) - k
         total = sum(cardPoints[r:])
         res = total
-        
         while r < len(cardPoints):
             total += (cardPoints[l] - cardPoints[r])
             res = max(res, total)
