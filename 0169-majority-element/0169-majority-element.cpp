4509ec8aaa5aa3edd0c95017b267ea1976a89d34
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-07 09:11:32 -0900

    Update 0169-majority-element.cpp

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -7,6 +7,7 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> counts;
         for (int num : nums) {
+            
             counts[num]++;
             if (counts[num] > nums.size() / 2) {
                 return num;
