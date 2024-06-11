1673bbb19b5d4222cc344c10458ebb32532da95c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-11 10:31:49 -0900

    Update 1920-build-array-from-permutation.py

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,5 +1,6 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        
         ans = [0]*len(nums)
         for i in range(0, len(nums)):
             ans[i] = nums[nums[i]]
