3a2ccfe7b8f44148d5d92466eb7ed90fe2dc6927
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-16 11:18:03 -0900

    Update 0237-delete-node-in-a-linked-list.py

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -10,6 +10,7 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        
         node.val = node.next.val
         node.next = node.next.next
 
