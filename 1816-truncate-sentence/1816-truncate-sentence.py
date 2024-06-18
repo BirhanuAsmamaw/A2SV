65ebdd60392aabff136d7badaffe6d98cf8a0a2c
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-17 22:24:01 -0900

    Time: 44 ms (6.56%), Space: 16.7 MB (10.72%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        new_s = s.split()
+        result = ""
+        for i in range(k):
+            result += new_s[i] + ' '
+                
+                
+        return result.strip()
