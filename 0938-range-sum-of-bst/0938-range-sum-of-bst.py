9db73eec69b1e7238b37f4966c47afca20db4331
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-18 13:01:51 -0900

    Update 0938-range-sum-of-bst.py

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        
         def dfs(node):
             nonlocal total
             if node:
