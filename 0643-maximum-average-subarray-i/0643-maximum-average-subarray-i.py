8cb4b1769d3c3037115d24e8ddbef515690e4771
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-19 10:19:19 -0900

    Update 0643-maximum-average-subarray-i.py

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -2,7 +2,6 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         result = float(-inf)
         current_sum,left = 0,0
-        
         for right in range(len(nums)):
             current_sum += nums[right]
             if right >= (k-1):
