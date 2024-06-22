2df1c322e9e362099d1edc2e8a1df3731883b680
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-22 09:46:14 -0900

    Update 0043-multiply-strings.java

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -3,11 +3,9 @@ class Solution {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
-
         int len1 = num1.length();
         int len2 = num2.length();
         int[] result = new int[len1 + len2];
-
         for (int i = len1 - 1; i >= 0; i--) {
             for (int j = len2 - 1; j >= 0; j--) {
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
