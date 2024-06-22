afb34d5d1f47fd7d4f05af19c2df9f580af6e5c1
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-22 09:45:41 -0900

    Update 2007-find-original-array-from-doubled-array.py

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -3,11 +3,9 @@ class Solution:
         n = len(changed)
         if n % 2:
             return []
-        
         count = Counter(changed)
         changed.sort()
         ans = []
-        
         for i in changed:
             if i == 0 and count[i] >= 2:
                 count[i] -= 2
@@ -16,7 +14,6 @@ class Solution:
                 count[i] -= 1
                 count[i*2] -= 1
                 ans.append(i)
-                
         return ans if len(ans) == n//2 else []
                 
                 
