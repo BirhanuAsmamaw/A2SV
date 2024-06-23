f780f034b93d5379b2eae72c54af6ea6adadbb08
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-23 10:27:07 -0900

    Update 1849-splitting-a-string-into-descending-consecutive-values.py

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -10,7 +10,6 @@ class Solution:
             return False
         for i in range(len(s)-1):
             lt_val = int(s[:i+1])
-            
             if dfs(i+1,lt_val):
                 return True
         return False
