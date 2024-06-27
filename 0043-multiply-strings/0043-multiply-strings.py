c0b4c9577415b0c68a78922f2820a6be44168fc1
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-27 04:34:43 -0900

    Update 0043-multiply-strings.py

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -2,10 +2,8 @@ class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == "0" or num2 == "0":
             return "0"
-
         len1, len2 = len(num1), len(num2)
         result = [0] * (len1 + len2)
-
         for i in range(len1 - 1, -1, -1):
             for j in range(len2 - 1, -1, -1):
                 mul = int(num1[i]) * int(num2[j])
