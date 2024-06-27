6b26aea8059a0299d61baf36bb2d186b36c4c8ee
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-27 05:00:46 -0900

    Time: 39 ms (56.62%), Space: 16.5 MB (48.74%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        
+        mismatch_count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                mismatch_count += 1
+                
+        return mismatch_count
