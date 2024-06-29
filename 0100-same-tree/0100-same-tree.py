928158b64eb4c6ccf0c48b69bf700b699f4cc91b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-29 06:56:21 -0900

    Update 0100-same-tree.py

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
         if not p and not q:
             return True 
         elif p and not q or q and not p:
