5ceb0b547a695aa8ea3f40a4fd13e8d065b670cd
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-01 06:07:51 -0900

    Update 0075-sort-colors.py

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,6 +6,5 @@ class Solution:
         for i in range(len(nums)):
             for j in range (len(nums)):
                 if nums[i] < nums[j] :
-                    nums[i], nums[j] = nums[j], nums[i]
-                    
+                    nums[i], nums[j] = nums[j], nums[i]          
         return nums  
