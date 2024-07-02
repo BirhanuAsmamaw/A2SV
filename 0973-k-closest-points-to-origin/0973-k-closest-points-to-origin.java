455ca0d12ea47a10a50ec7322d09afb60eaaa480
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-02 12:15:38 -0900

    Update 0973-k-closest-points-to-origin.java

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -5,7 +5,6 @@ class Solution {
                 return Integer.compare(a[0]*a[0] + a[1]*a[1], b[0]*b[0] + b[1]*b[1]);
             }
         });
-
         return Arrays.copyOfRange(points, 0, k);
     }
 }
