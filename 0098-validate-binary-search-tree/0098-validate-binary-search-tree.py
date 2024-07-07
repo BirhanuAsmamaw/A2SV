de19a62f7c46d0de311951293425a7aff323cc16
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-07 11:38:37 -0900

    Update 0098-validate-binary-search-tree.py

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -10,8 +10,7 @@ class Solution:
             if not node:
                 return True
             if not(node.val > left and node.val < right):
-                return False
-                
+                return False  
             return  (validate(node.left,left,node.val) and validate(node.right,node.val,right))  
         return validate(root,-inf,inf)
             
