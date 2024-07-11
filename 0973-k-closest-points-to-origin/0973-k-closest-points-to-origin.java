eaa2fa9501e8038dabfe4054f044b65394137f82
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-11 11:10:14 -0900

    Update 0973-k-closest-points-to-origin.java

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         Arrays.sort(points, new Comparator<int[]>() {
+            
             public int compare(int[] a, int[] b) {
                 return Integer.compare(a[0]*a[0] + a[1]*a[1], b[0]*b[0] + b[1]*b[1]);
+                
             }
         });
         return Arrays.copyOfRange(points, 0, k);
