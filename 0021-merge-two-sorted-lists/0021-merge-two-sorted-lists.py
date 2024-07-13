bd5849d5abefd3a7e7e3ecf226c81b2d9db0f84b
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-13 11:52:52 -0900

    Update 0021-merge-two-sorted-lists.py

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -8,6 +8,7 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         node = ListNode()
         tail = node
+        
         while list1 and list2:
             if list1.val < list2.val:
                 tail.next = list1
@@ -16,6 +17,7 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
+            
         if list1:
             tail.next = list1
         elif list2:
