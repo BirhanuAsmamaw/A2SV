b1097038449757ae771a51ea3341de2a1b468683
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-14 06:41:20 -0900

    Update 0075-sort-colors.py

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         for i in range(len(nums)):
             for j in range (len(nums)):
                 if nums[i] < nums[j] :
