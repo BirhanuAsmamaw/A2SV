863e0aaaa1f21def1c69460135ec7e516c12dd54
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-18 22:23:03 -0900

    Update 1512-number-of-good-pairs.py

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,6 +1,5 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        
         count = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
