e1e46658ab63ff58efd63980fc406e050451cd77
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-07-20 10:49:26 -0900

    Update 0083-remove-duplicates-from-sorted-list.py

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
