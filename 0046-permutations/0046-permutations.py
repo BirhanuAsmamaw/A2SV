e7a5f27270ada1cfb04e413a476a23e1f720d796
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-05 09:37:10 -0900

    Update 0046-permutations.py

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,5 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
         sett = set()
         while (len(sett) != factorial(len(nums))):
             shuffle(nums)
