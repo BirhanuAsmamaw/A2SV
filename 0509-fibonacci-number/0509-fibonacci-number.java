f8187709d4984e610b0b6bfbdd11f94a3f3a9970
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-16 09:26:31 -0900

    Update 0509-fibonacci-number.java

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,11 @@
 public class Solution {
     public int fib(int n) {
+        
         if (n == 0 || n == 1) {
             return n;
         } else {
             return fib(n - 1) + fib(n - 2);
         }
+        
     }
 }
