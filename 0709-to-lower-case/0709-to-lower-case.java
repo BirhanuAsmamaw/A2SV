9ea937eb1712bb36946de020b38fa17ac8f3fec2
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-16 09:27:00 -0900

    Update 0709-to-lower-case.java

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
+        
         StringBuilder result = new StringBuilder();
         for (char c : s.toCharArray()) {
             if (Character.isUpperCase(c)) {
