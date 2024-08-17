fd823359140325c079b4588f5f718a48e40f6a2a
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-17 05:10:50 -0900

    Update 0486-predict-the-winner.py

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -5,7 +5,7 @@ class Solution:
                 return nums[start]
             else:
                 return max(nums[start] - predict(nums,start +1,end), nums[end] - predict(nums,start,end-1))
-        
+                
         result = predict(nums, 0 ,len(nums) - 1)
         
         if result >= 0:
