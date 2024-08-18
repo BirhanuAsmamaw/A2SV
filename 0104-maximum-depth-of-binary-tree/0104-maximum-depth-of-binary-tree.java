88b60f74550b1d6027c81bb4a92540a02a0b073e
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-18 10:10:13 -0900

    Update 0104-maximum-depth-of-binary-tree.java

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,12 +13,14 @@
  *     }
  * }
  */
+
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
 }
+
 class Solution {
     
     public int maxDepth(TreeNode root) {
