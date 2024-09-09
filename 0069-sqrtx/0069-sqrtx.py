f8e43db165b2911821cff08e41fc37ffc53cc077
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-09-09 12:06:28 -0900

    Update 0069-sqrtx.py

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,7 +5,6 @@ class Solution:
         start, end = 1, x
         while start <= end:
             mid = (start + end) // 2
-            
             if mid * mid == x:
                 return mid
             elif mid * mid < x:
