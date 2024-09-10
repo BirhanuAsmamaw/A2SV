23ed3a9c0f4fea65221940b193758aeb7c7413b5
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-09-09 23:24:57 -0900

    Update 0155-min-stack.py

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,7 +2,7 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.minstack = []
-
+        
     def push(self, val: int) -> None:
         self.stack.append(val)
         if self.minstack and val > self.minstack[-1]:
