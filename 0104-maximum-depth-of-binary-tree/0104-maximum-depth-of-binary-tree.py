de891acafbbdf0786a8366abffc68131e6709a00
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-09-13 08:06:59 -0900

    Update 0104-maximum-depth-of-binary-tree.py

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -7,7 +7,6 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def max_depth(root):
-            
             if not root:
                 return 0
             return 1 + max(max_depth(root.left), max_depth(root.right))
