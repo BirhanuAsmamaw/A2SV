fd963fc0cb3a0115c7054823b5941109142ca50a
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-03 10:32:06 -0900

    Update 0077-combinations.py

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,6 +1,5 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
         result = []  
         def backTracking(start , combination):
             if len(combination) == k:
