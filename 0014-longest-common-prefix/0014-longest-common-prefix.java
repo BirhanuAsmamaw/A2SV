2da4bb3dd27672f120a140d842ebcc23c2c0f4cf
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-10 00:19:49 +0300

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,5 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        
         if (strs == null || strs.length == 0) return "";
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
