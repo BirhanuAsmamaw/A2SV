0e20a719b8a76647bba11a1cbf03030341d2f7fe
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-21 02:48:39 +0300

    Update 0237-delete-node-in-a-linked-list.py

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -5,7 +5,6 @@ class ListNode:
         self.next = None
 class Solution:
     def deleteNode(self, node):
-        
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
