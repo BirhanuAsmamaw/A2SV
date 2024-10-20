33e486b988b105a809bfcf22d8d269018ab35506
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-21 02:55:46 +0300

    Update 0771-jewels-and-stones.py

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,6 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         Tot_jewel = 0
-        
         for i in stones:
             if i in jewels:
                 Tot_jewel += 1  
