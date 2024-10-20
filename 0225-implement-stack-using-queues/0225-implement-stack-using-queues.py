2042fc5cfcc41f6b87352f7dd9ae97ae74961524
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-10-21 02:48:09 +0300

    Update 0225-implement-stack-using-queues.py

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,21 +1,17 @@
 class MyStack:
     def __init__(self):
         self.stack = []
-        self.size = 0
-        
+        self.size = 0 
     def push(self, x: int) -> None:
         self.stack.append(x)
         self.size += 1
-
     def pop(self) -> int:
         if not self.empty():   
             self.size -= 1
             return self.stack.pop()   
-
     def top(self) -> int:
         if self.stack:
             return self.stack[ - 1]
-
     def empty(self) -> bool:
         if self.size == 0:
             return True
